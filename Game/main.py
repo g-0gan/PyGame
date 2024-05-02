3cf3a7e8b5103eabbbf5ed3854a946e55d0434cf
slothGeorge <gosh20002@gmail.com>
2024-05-02 15:05:03 +0400

    refactor: making functions for changing colors by keys as a solution of DRY problem in previous version

--- Game/main.py
+++ Game/main.py
@@ -7,6 +7,9 @@ COLORS = [
     ['aqua', 'indigo', 'brown'],
     ['coral', 'crimson', 'yellowgreen'],
 ]
+
+COLOR_LEN_COLUMN = len(COLORS)
+COLOR_LEN_LINE = len(COLORS[0])
 FPS = 25
 
 
@@ -17,8 +20,20 @@ async def main():
 
     color_index_column = 0
     color_index_line = 0
-    color_len_column = len(COLORS)
-    color_len_line = len(COLORS[0])
+
+    def change_color_line(current_index: int, increment: int):
+        current_index = (current_index + increment) % COLOR_LEN_LINE
+        new_color = COLORS[color_index_column][current_index]
+        screen.fill(new_color)
+        pygame.display.update()
+        return current_index
+
+    def change_color_column(current_index: int, increment: int):
+        current_index = (current_index + increment) % COLOR_LEN_COLUMN
+        new_color = COLORS[current_index][color_index_line]
+        screen.fill(new_color)
+        pygame.display.update()
+        return current_index
 
     while True:  # EVENT LOOP
         for event in pygame.event.get():
@@ -26,36 +41,23 @@ async def main():
                 continue
 
             if event.key == pygame.K_a:
-                color_index_line = (color_index_line - 1) % color_len_line
-                new_color = COLORS[color_index_column][color_index_line]
-                screen.fill(new_color)
-                pygame.display.update()
+                color_index_line = change_color_line(color_index_line, -1)
 
             if event.key == pygame.K_d:
-                color_index_line = (color_index_line + 1) % color_len_line
-                new_color = COLORS[color_index_column][color_index_line]
-                screen.fill(new_color)
-                pygame.display.update()
+                color_index_line = change_color_line(color_index_line, 1)
 
             if event.key == pygame.K_w:
-                color_index_column = (color_index_column + 1) % color_len_column
-                new_color = COLORS[color_index_column][color_index_line]
-                screen.fill(new_color)
-                pygame.display.update()
+                color_index_column = change_color_column(color_index_column, -1)
 
             if event.key == pygame.K_s:
-                color_index_column = (color_index_column - 1) % color_len_column
-                new_color = COLORS[color_index_column][color_index_line]
-                screen.fill(new_color)
-                pygame.display.update()
+                color_index_column = change_color_column(color_index_column, 1)
 
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
 
-        clock.tick(FPS)  # Frames per second, 25 fps
+        clock.tick(FPS)
         await asyncio.sleep(0)
-
 
 if __name__ == '__main__':
     asyncio.run(main())
