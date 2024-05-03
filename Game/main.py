d52a8f09c0e30450e1e6820ca64d6951e7f317b7
slothGeorge <gosh20002@gmail.com>
2024-05-03 16:22:32 +0400

    refactor: combining two functions that were responsible for changing a row and column into one

--- Game/main.py
+++ Game/main.py
@@ -21,19 +21,19 @@ async def main():
     color_index_column = 0
     color_index_line = 0
 
-    def change_color_line(current_index: int, increment: int):
-        current_index = (current_index + increment) % COLOR_LEN_LINE
-        new_color = COLORS[color_index_column][current_index]
-        screen.fill(new_color)
-        pygame.display.update()
-        return current_index
-
-    def change_color_column(current_index: int, increment: int):
-        current_index = (current_index + increment) % COLOR_LEN_COLUMN
-        new_color = COLORS[current_index][color_index_line]
-        screen.fill(new_color)
-        pygame.display.update()
-        return current_index
+    def change_color(current_index: int, increment: int, is_line: bool):
+        if is_line:
+            current_index = (current_index + increment) % COLOR_LEN_LINE
+            new_color = COLORS[color_index_column][current_index]
+            screen.fill(new_color)
+            pygame.display.update()
+            return current_index
+        else:
+            current_index = (current_index + increment) % COLOR_LEN_COLUMN
+            new_color = COLORS[current_index][color_index_line]
+            screen.fill(new_color)
+            pygame.display.update()
+            return current_index
 
     while True:  # EVENT LOOP
         for event in pygame.event.get():
@@ -41,16 +41,16 @@ async def main():
                 continue
 
             if event.key == pygame.K_a:
-                color_index_line = change_color_line(color_index_line, -1)
+                color_index_line = change_color(color_index_line, -1, True)
 
             if event.key == pygame.K_d:
-                color_index_line = change_color_line(color_index_line, 1)
+                color_index_line = change_color(color_index_line, 1, True)
 
             if event.key == pygame.K_w:
-                color_index_column = change_color_column(color_index_column, -1)
+                color_index_column = change_color(color_index_column, -1, False)
 
             if event.key == pygame.K_s:
-                color_index_column = change_color_column(color_index_column, 1)
+                color_index_column = change_color(color_index_column, 1, False)
 
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
